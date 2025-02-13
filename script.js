1e082ec1c49c44562d0769eda250d5fff86ddf18
Carlos Rodriguez <r.carlos@correo.unimet.edu.ve>
2025-02-13 19:10:08 -0400

    backend extensions

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    //Datos del programa (Variables)
     const botones = ["red", "yellow", "blue", "green"]; //Lista de botones
     let instrucciones = []; //Pila de instrucciones del programa
     let secuenciaCliente = []; //Pila de instrucciones cliente
     let puntaje = 0 //Puntaje cliente
     let cliente = ""; //Nombre del Cliente
+    let estadoJuego = false
 
+    //Componentes
     const botonEmpezar = document.getElementById("start");
+    const botonParar = document.getElementById("end");
+    const botonCambiarNombre = document.getElementById("change-name");
     const botonesDiv = document.querySelectorAll(".color");
+    const scoreLabel = document.getElementById("puntaje")
+    const clientLabel = document.getElementById("clientName");
 
+    //Eventos
     botonEmpezar.addEventListener("click", startGame);
+    botonCambiarNombre.addEventListener("click", changeClient);
+    botonParar.addEventListener("click", stopGame)
 
     /*
     Funciones del Juego:
-    - startGame: Empieza juego dejando todo de default
-    - nextLevel: Asigna nuevo patron a partir del anterior
-    - changeClient: Cuando se presiona el boton de cambiar nombre este se encarga de modificar el nombre
-    - playSequence: Ejecutar secuencia a mostrar 
-    - checkSequence: Verifica la secuencia del cliente con la establecida, de ser iguales el cliente obtiene 5 puntos, de no serlo se termina el juego 
+    - startGame: Empieza juego dejando todo de default.
+
+    - nextLevel: Asigna nuevo patron a partir del anterior.
+
+    - changeClient: Cuando se presiona el boton de cambiar nombre este se encarga de modificar el nombre.
+
+    - playSequence: Ejecutar secuencia a mostrar. 
+
+    - checkSequence: Verifica la secuencia del cliente con la establecida, de ser iguales el cliente obtiene 5 puntos, de no serlo se termina el juego. 
+
+    -stopGame: cuando se presina el boton parar se termina el juego.
+
+    -updateScore: actualiza la puntuacion graficamente (reflejandolo en el div).
     */
 
-    //
     function startGame() { 
         instrucciones = []
         secuenciaCliente = [];
         puntaje = 0;
-        cliente = prompt("Ingresa tu nombre");
+        updateScore()
+        if(!cliente){ //Si no se ha definido nombre se solicita que se haga
+            changeClient();
+        }
+        estadoJuego = true
         nextLevel();
+    }
+
+    function changeClient(){
+        cliente = prompt("Ingrese su nombre");
+        clientLabel.textContent = cliente;
     }
 
     function nextLevel() {
         secuenciaCliente = [];
-        puntaje = puntaje + 5; //agrega 5 puntos si se gano un nivel
         const randomColor = botones[Math.floor(Math.random() * botones.length)]; //Se elige un color al  de la lista usando los metodos de Math
         instrucciones.push(randomColor);
         playSequence()
@@ -49,11 +75,24 @@ document.addEventListener("DOMContentLoaded", () => {
     function checkSequence() {
         const currentIndex  = secuenciaCliente.length - 1;
         if (secuenciaCliente[currentIndex] !== instrucciones[currentIndex]){ //Verificamos si las ultimas instrucciones de cada una es la misma (Mejorar)
-            alert("Perdiste! Juego terminado. Llegaste hasta " + puntaje + " puntos");       
+            alert("Perdiste " + cliente + "! Juego terminado. Llegaste hasta " + puntaje + " puntos");       
             startGame();
         } else if(secuenciaCliente.length === instrucciones.length){ // verificamso si se instrodujo la cantidad de instruccines
+            puntaje = puntaje + 5; //agrega 5 puntos si se gano un nivel
+            updateScore();
             setTimeout(nextLevel, 1000);
         }
+    }
+
+    function stopGame(){
+        estadoJuego = false;
+        alert("Juego terminado, gracias por jugar " + cliente + " tu puntuacion ha sido de " + puntaje + " puntos.");
+        puntaje = 0;
+        updateScore();
+    }
+
+    function updateScore(){
+        scoreLabel.textContent = puntaje
     }
 
         /*
@@ -78,6 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //funcion anonima que busca el color presionado y le agrega a la secuencia d ebotones del cliente
     botonesDiv.forEach(div => {
         div.addEventListener("click", (event) => {
+            if(!estadoJuego) return; //Condicional que verifica si todavia se esta jugando
             const color = event.target.id; //buscamos el boton presionado
             secuenciaCliente.push(color); //agregamos color presionado
             playSound(color);
